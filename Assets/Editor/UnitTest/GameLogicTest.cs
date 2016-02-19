c8f6f092157560bfd9120964c20d07892f029a2c
ilkin Balkanay <ilkinulas@gmail.com>
2016-02-19 06:29:07 +0200

    GameLogic implementation with unit tests

---
+++ Assets/Editor/UnitTest/GameLogicTest.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+namespace net.peakgames.codebreaker {
+	
+	public class GameLogicTest {
+
+		[Test]
+		public void SuccessfulGuess() {
+			GameLogic mastermind = new GameLogic (new int[] {1, 2, 3, 4});
+			int [] guess = new int [] {1, 2, 3, 4};
+			Result result = mastermind.Check (guess);
+			Assert.AreEqual (4, result.whiteCount);
+			Assert.AreEqual (0, result.blackCount);
+			Assert.IsTrue (result.IsCorrect ());
+		}
+
+		[Test]
+		public void AllGuessesAreMissing() {
+			GameLogic mastermind = new GameLogic (new int[] {1, 2, 3, 4});
+			int [] guess = new int [] {5, 6, 7, 8};
+			Result result = mastermind.Check (guess);
+			Assert.AreEqual (0, result.whiteCount);
+			Assert.AreEqual (0, result.blackCount);
+			Assert.IsFalse (result.IsCorrect ());
+		}
+
+		[Test]
+		public void AllGuessesAreBlack() {
+			GameLogic mastermind = new GameLogic (new int[] {1, 2, 3, 4});
+			int [] guess = new int [] {2, 1, 4, 3,};
+			Result result = mastermind.Check (guess);
+			Assert.AreEqual (0, result.whiteCount);
+			Assert.AreEqual (4, result.blackCount);
+			Assert.IsFalse (result.IsCorrect ());
+		}
+
+		[Test]
+		public void TwoBlacksAndTwoWhites() {
+			GameLogic mastermind = new GameLogic (new int[] {1, 2, 3, 4});
+			int [] guess = new int [] {1, 2 , 4 , 3};
+			Result result = mastermind.Check (guess);
+			Assert.AreEqual (2, result.whiteCount);
+			Assert.AreEqual (2, result.blackCount);
+			Assert.IsFalse (result.IsCorrect ());
+		}
+
+		[Test]
+		public void SameDigitSolutionAndWrongGuess() {
+			GameLogic mastermind = new GameLogic (new int[] {0, 0, 0, 0});
+			int [] guess = new int [] {1, 2 , 4 , 3};
+			Result result = mastermind.Check (guess);
+			Assert.AreEqual (0, result.whiteCount);
+			Assert.AreEqual (0, result.blackCount);
+			Assert.IsFalse (result.IsCorrect ());
+		}
+
+		[Test]
+		public void SameDigitSolutionAndCorrectGuess() {
+			GameLogic mastermind = new GameLogic (new int[] {0, 0, 0, 0});
+			int [] guess = new int [] {0, 0 ,0 ,0};
+			Result result = mastermind.Check (guess);
+			Assert.AreEqual (4, result.whiteCount);
+			Assert.AreEqual (0, result.blackCount);
+			Assert.IsTrue (result.IsCorrect ());
+		}
+
+		[Test]
+		public void SameDigitGuessWithWhites() {
+			GameLogic mastermind = new GameLogic (new int[] {7, 4, 8, 1});
+			int [] guess = new int [] {7, 7, 7 ,7};
+			Result result = mastermind.Check (guess);
+			Assert.AreEqual (1, result.whiteCount);
+			Assert.AreEqual (0, result.blackCount);
+			Assert.IsFalse (result.IsCorrect ());
+		}
+	}
+}
---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace net.peakgames.codebreaker {
+	
+	public class GameLogic {
+		
+		public const int MAX_NUMBERS = 4;
+		private readonly int [] solution;
+		public enum MatchType {NONE, WHITE, BLACK}
+
+		public GameLogic(int [] solution) {
+			this.solution = solution;
+		}
+
+		public Result Check(int [] guess) {
+			int whites = 0;
+			int blacks = 0;
+			List<int> processed = new List<int> ();
+			for (int i = 0; i < MAX_NUMBERS; i++) {
+				if (guess [i] == solution [i]) {
+					whites++;
+					processed.Add (i);
+				}
+			}
+
+			for (int i = 0; i < MAX_NUMBERS; i++) {
+				for (int j = 0; j < MAX_NUMBERS; j++) {
+					int aGuess = guess [i];
+					if (!processed.Contains (j) && aGuess == solution [j]) {
+						processed.Add (j);
+						blacks++;
+						break;
+					}
+				}
+			}
+			return new Result(whites, blacks);
+		}
+
+		private int [] CreateEmptyResult () {
+			return new int[] { 
+				(int)MatchType.NONE, 
+				(int)MatchType.NONE, 
+				(int)MatchType.NONE, 
+				(int)MatchType.NONE
+			};
+		}
+	}
+
+	public class Result {
+		public readonly int whiteCount;
+		public readonly int blackCount;
+
+		public Result(int white, int black) {
+			this.whiteCount = white;
+			this.blackCount = black;
+		}
+
+		public bool IsCorrect() {
+			return this.whiteCount == GameLogic.MAX_NUMBERS;
+		}
+	}
+}
